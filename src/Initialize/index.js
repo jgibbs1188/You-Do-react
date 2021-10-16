c242adba0c5350f94d8203e9539152e3fd246b2c
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-10-16 12:21:17 -0500

    able to route to home/view completed and delete from both pages

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getTodos } from '../api/data/todoData';
-import Todo from '../components/Todo';
+import Routes from '../routes';
+import Navigation from '../components/Navigation';
 import TodoForm from '../components/TodoForm';
 
 function Initialize() {
@@ -8,21 +9,15 @@ function Initialize() {
   const [editItem, setEditItem] = useState({});
 
   useEffect(() => {
-    getTodos().then(setTodos);
+    getTodos(false).then(setTodos);
   }, []);
 
   return (
     <>
+      <Navigation />
       <h1>You-Do</h1>
       <TodoForm obj={editItem} setTodos={setTodos} setEditItem={setEditItem} />
-      {todos.map((todo) => (
-        <Todo
-          key={todo.firebaseKey}
-          taco={todo}
-          setTodos={setTodos}
-          setEditItem={setEditItem}
-        />
-      ))}
+      <Routes todos={todos} setTodos={setTodos} setEditItem={setEditItem} />
     </>
   );
 }
--- src/api/data/todoData.js
+++ src/api/data/todoData.js
@@ -3,10 +3,23 @@ import firebaseConfig from '../apiKeys';
 
 const dbUrl = firebaseConfig.databaseURL;
 
-const getTodos = () => new Promise((resolve, reject) => {
+const getTodos = (value) => new Promise((resolve, reject) => {
   axios
-    .get(`${dbUrl}/todos.json?orderBy="complete"&equalTo=false`)
+    .get(`${dbUrl}/todos.json?orderBy="complete"&equalTo=${value}`)
     .then((response) => resolve(Object.values(response.data)))
+    .catch(reject);
+});
+
+const getCompletedTodos = () => new Promise((resolve, reject) => {
+  getTodos(true)
+    .then((todoArray) => resolve(todoArray))
+    .catch(reject);
+});
+
+const deleteCompletedTodo = (firebaseKey) => new Promise((resolve, reject) => {
+  axios
+    .delete(`${dbUrl}/todos/${firebaseKey}.json`)
+    .then(() => getCompletedTodos().then(resolve))
     .catch(reject);
 });
 
@@ -18,7 +31,7 @@ const createTodo = (obj) => new Promise((resolve, reject) => {
       axios
         .patch(`${dbUrl}/todos/${firebaseKey}.json`, { firebaseKey })
         .then(() => {
-          getTodos().then(resolve);
+          getTodos(false).then(resolve);
         });
     })
     .catch(reject);
@@ -27,17 +40,22 @@ const createTodo = (obj) => new Promise((resolve, reject) => {
 const deleteTodo = (firebaseKey) => new Promise((resolve, reject) => {
   axios
     .delete(`${dbUrl}/todos/${firebaseKey}.json`)
-    .then(() => getTodos().then(resolve))
+    .then(() => getTodos(false).then(resolve))
     .catch(reject);
 });
 
 const updateTodo = (todoObj) => new Promise((resolve, reject) => {
   axios
     .patch(`${dbUrl}/todos/${todoObj.firebaseKey}.json`, todoObj)
-    .then(() => getTodos().then(resolve))
+    .then(() => getTodos(false).then(resolve))
     .catch(reject);
 });
 
 export {
-  getTodos, createTodo, deleteTodo, updateTodo,
+  getTodos,
+  createTodo,
+  deleteTodo,
+  updateTodo,
+  getCompletedTodos,
+  deleteCompletedTodo,
 };
